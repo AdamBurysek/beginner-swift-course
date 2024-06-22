ca0d0dc3aae200818e6c3ef596be18ffd8c9c77c
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-22 11:12:41 +0200

    add variables

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -1,3 +1,55 @@
 import Foundation
 
+// Let cant be reassigned
+// Var can be reassigned
 
+let myName = "Foo"
+let yourName = "Bar"
+
+var names = [
+    myName,
+    yourName
+]
+
+names.append("Bar")
+names.append("Baz")
+
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = [
+ "Foo",
+  "Bar"]
+
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+let oldArray = NSMutableArray(
+array: ["Foo",
+        "Bar"
+       ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
+
+let someNames = NSMutableArray(
+array: ["Foo",
+        "Bar"
+       ]
+)
+
+func changeTheArray(_ array: NSArray) {
+    let arrayTwo = array as! NSMutableArray
+    arrayTwo.add("Baz")
+}
+changeTheArray(someNames)
+someNames
